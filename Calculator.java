52106edbc1a7d57c1d58b31f4ce0621eafa09a16
minsoek132 <63045030+minsoek132@users.noreply.github.com>
2024-10-14 01:03:51 +0900

    github 처음 깔아서 해보기

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;;
+
+public class Calculator extends JFrame {
+    setTitle("계산기");
+    setSize(300, 200);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
