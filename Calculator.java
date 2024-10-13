95dcf8ca085b7afac74263c5096fcd186298ba9d
minsoek132 <63045030+minsoek132@users.noreply.github.com>
2024-10-14 01:07:21 +0900

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,15 @@ import java.awt.*;
 import javax.swing.*;;
 
 public class Calculator extends JFrame {
-    setTitle("계산기");
-    setSize(300, 200);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setVisible(true);
+
+    public Calculator(){
+        setTitle("계산기");
+        setSize(300, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+    }
 
     
     public static void main(String[] args) {
